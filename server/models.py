6b72e8cb1bedf443a2f98043260d6aa705d739de
student <kmac.kimaru93@gmail.com>
2024-07-29 15:07:38 +0300

    creating user model

---
+++ server/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy_serializer import SerializerMixin
+from config import bcrypt
+
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(255), nullable=False)
+    last_name = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    _password_hash = db.Column(db.String(255), nullable=False)
+
+   
